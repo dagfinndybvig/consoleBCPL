2245d8ac3cd0ca21f108ca0de99122c4e317165f
Dagfinn Dybvig <dagfinn.dybvig@gmail.com>
2026-01-22 10:04:34 +0000

    Coroutines cross compile

--- bcpl-rust-console/bcpl-with-coroutines/compile.bat
+++ bcpl-rust-console/bcpl-with-coroutines/compile.bat
@@ -1,5 +1,7 @@
 @echo off
 setlocal
+set "ROOT=%~dp0"
+pushd "%ROOT%" >nul
 
 REM BCPL Compiler Script (Rust coroutines version)
 REM Compiles and runs a BCPL program using the coroutine-enabled Rust INTCODE interpreter
@@ -16,18 +18,46 @@ if not exist "%~1" (
 )
 
 REM Concatenate syni and trni
-copy /b syni+trni synitrni >nul
+set "ICINT=.\target\x86_64-pc-windows-gnu\release\icint.exe"
+set "SYN=.\syni"
+set "TRN=.\trni"
+set "CGI=.\cgi"
+
+if not exist "%SYN%" set "SYN=..\syni"
+if not exist "%TRN%" set "TRN=..\trni"
+if not exist "%CGI%" set "CGI=..\cgi"
+
+if not exist "%ICINT%" (
+    echo Error: icint.exe not found at "%ICINT%"
+    exit /b 1
+)
+if not exist "%SYN%" (
+    echo Error: syni not found
+    exit /b 1
+)
+if not exist "%TRN%" (
+    echo Error: trni not found
+    exit /b 1
+)
+if not exist "%CGI%" (
+    echo Error: cgi not found
+    exit /b 1
+)
+
+REM Concatenate syni and trni
+copy /b "%SYN%"+"%TRN%" synitrni >nul
 
 REM Compile BCPL to OCODE
 echo Compiling %~1 to OCODE...
-."\target\x86_64-pc-windows-gnu\release\icint.exe" synitrni -i%~1
+"%ICINT%" synitrni -i%~1
 
 REM Compile OCODE to INTCODE
 echo Compiling OCODE to INTCODE...
-."\target\x86_64-pc-windows-gnu\release\icint.exe" cgi -iOCODE
+"%ICINT%" "%CGI%" -iOCODE
 
 REM Run INTCODE
 echo Running INTCODE...
-."\target\x86_64-pc-windows-gnu\release\icint.exe" INTCODE %2 %3
+"%ICINT%" INTCODE %2 %3
 
+popd >nul
 endlocal
---
+++ bcpl-rust-console/bcpl-with-coroutines/target/x86_64-pc-windows-gnu/release/icint.d
@@ -0,0 +1,1 @@
+/workspaces/consoleBCPL/bcpl-rust-console/bcpl-with-coroutines/target/x86_64-pc-windows-gnu/release/icint.exe: /workspaces/consoleBCPL/bcpl-rust-console/bcpl-with-coroutines/src/main.rs
